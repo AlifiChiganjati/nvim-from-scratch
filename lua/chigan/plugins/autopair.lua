defc0ec2789403731acd65a5699d954aec8a444e
Alifi Chiganjati <alifichiganjati@gmail.com>
2025-08-09 12:22:13 +0800

    FEAT: change nvim-cpm to blink-cmp and lsp/ move in plugins

--- lua/chigan/plugins/autopair.lua
+++ lua/chigan/plugins/autopair.lua
@@ -1,31 +1,25 @@
 return {
-  "windwp/nvim-autopairs",
-  event = { "InsertEnter" },
-  dependencies = {
-    "hrsh7th/nvim-cmp",
-  },
-  config = function()
-    -- import nvim-autopairs
-    local autopairs = require("nvim-autopairs")
-
-    -- configure autopairs
-    autopairs.setup({
-      check_ts = true, -- enable treesitter
-      ts_config = {
-        lua = { "string" }, -- don't add pairs in lua string treesitter nodes
-        javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
-        java = false, -- don't check treesitter on java
-      },
-    })
-
-    -- import nvim-autopairs completion functionality
-    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-
-    -- import nvim-cmp plugin (completions plugin)
-    local cmp = require("cmp")
-
-    -- make autopairs and completion work together
-    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-  end,
+	"saghen/blink.pairs",
+	version = "*",
+	dependencies = "saghen/blink.download",
+	opts = {
+		mappings = {
+			enabled = true,
+			disabled_filetypes = {},
+			pairs = {}, -- add overrides if needed
+		},
+		highlights = {
+			enabled = true,
+			groups = {
+				"BlinkPairsOrange",
+				"BlinkPairsPurple",
+				"BlinkPairsBlue",
+			},
+			matchparen = {
+				enabled = true,
+				group = "BlinkPairsMatchParen",
+			},
+		},
+		debug = false,
+	},
 }
-
---
+++ lua/chigan/plugins/lsp-config.lua
@@ -0,0 +1,163 @@
+return {
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		{ "williamboman/mason-lspconfig.nvim" },
+		{ "saghen/blink.cmp", opts = {} },
+		{ "rafamadriz/friendly-snippets" },
+		{ "antosha417/nvim-lsp-file-operations", config = true },
+		{ "folke/neodev.nvim" },
+	},
+	config = function()
+		local lspconfig = require("lspconfig")
+		local mason_lspconfig = require("mason-lspconfig")
+		local capabilities = require("blink.cmp").get_lsp_capabilities()
+		local keymap = vim.keymap
+
+		-- Keymaps on attach
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+			callback = function(ev)
+				local opts = { buffer = ev.buf, silent = true }
+
+				opts.desc = "Show LSP references"
+				keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+
+				opts.desc = "Go to declaration"
+				keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+
+				opts.desc = "Show LSP definitions"
+				keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+
+				opts.desc = "Show LSP implementations"
+				keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+
+				opts.desc = "Show LSP type definitions"
+				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+
+				opts.desc = "See available code actions"
+				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+
+				opts.desc = "Smart rename"
+				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+
+				opts.desc = "Show buffer diagnostics"
+				keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+
+				opts.desc = "Show line diagnostics"
+				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+
+				opts.desc = "Go to previous diagnostic"
+				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+
+				opts.desc = "Go to next diagnostic"
+				keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+
+				opts.desc = "Show documentation for what is under cursor"
+				keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+				opts.desc = "Restart LSP"
+				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+			end,
+		})
+
+		-- Diagnostic UI
+		vim.diagnostic.config({
+			virtual_text = {
+				prefix = "●",
+				spacing = 2,
+				source = "if_many",
+				severity = { min = vim.diagnostic.severity.ERROR },
+			},
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "",
+					[vim.diagnostic.severity.WARN] = "",
+					[vim.diagnostic.severity.INFO] = "",
+					[vim.diagnostic.severity.HINT] = "󰠠",
+				},
+			},
+			underline = true,
+			update_in_insert = false,
+			severity_sort = true,
+		})
+
+		-- Mason LSP setup with blink.cmp capabilities
+		mason_lspconfig.setup({
+			handlers = {
+				function(server_name)
+					lspconfig[server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
+				["svelte"] = function()
+					lspconfig["svelte"].setup({
+						capabilities = capabilities,
+						on_attach = function(client, _)
+							vim.api.nvim_create_autocmd("BufWritePost", {
+								pattern = { "*.js", "*.ts" },
+								callback = function(ctx)
+									client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+								end,
+							})
+						end,
+					})
+				end,
+				["graphql"] = function()
+					lspconfig["graphql"].setup({
+						capabilities = capabilities,
+						filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+					})
+				end,
+				["emmet_ls"] = function()
+					lspconfig["emmet_ls"].setup({
+						capabilities = capabilities,
+						filetypes = {
+							"astro",
+							"css",
+							"eruby",
+							"html",
+							"htmldjango",
+							"javascriptreact",
+							"less",
+							"pug",
+							"sass",
+							"scss",
+							"svelte",
+							"typescriptreact",
+							"vue",
+							"blade",
+							"php",
+						},
+					})
+				end,
+				["lua_ls"] = function()
+					lspconfig["lua_ls"].setup({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								diagnostics = { globals = { "vim" } },
+								completion = { callSnippet = "Replace" },
+							},
+						},
+					})
+				end,
+				["intelephense"] = function()
+					lspconfig["intelephense"].setup({
+						capabilities = capabilities,
+						filetypes = { "php", "blade", "php_only" },
+						settings = {
+							intelephense = {
+								filetypes = { "php", "blade", "php_only" },
+								files = {
+									associations = { "*.php", "*.blade.php" },
+									maxSize = 5000000,
+								},
+							},
+						},
+					})
+				end,
+			},
+		})
+	end,
+}
